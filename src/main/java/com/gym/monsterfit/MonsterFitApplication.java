4b23e311a4566c7590f765d5ef12284d96a25995
Ingrid Florez <ingrid.florez0226@gmail.com>
2023-05-20 20:42:35 -0500

    primer commit

---
+++ src/main/java/com/gym/monsterfit/MonsterFitApplication.java
@@ -0,0 +1,13 @@
+package com.gym.monsterfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonsterFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonsterFitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gym/monsterfit/MonsterFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gym.monsterfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonsterFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
