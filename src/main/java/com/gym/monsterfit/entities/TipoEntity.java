4c56616840698e62803127bdf67dd599f49932e7
josegabrielfc1152085 <josegabrielfc@ufps.edu.co>
2023-05-22 15:18:02 -0500

    testing something

--- src/main/java/com/gym/monsterfit/entities/TipoEntity.java
+++ src/main/java/com/gym/monsterfit/entities/TipoEntity.java
@@ -9,7 +9,7 @@ public class TipoEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	private String nombre;
+	private String nombre; //Nombre del Tipo de rutina
 	
 	
 	public TipoEntity(Integer id, String tipo) {
@@ -20,12 +20,15 @@ public class TipoEntity {
 	public Integer getId() {
 		return id;
 	}
+	
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
 	public String getTipo() {
 		return nombre;
 	}
+	
 	public void setTipo(String tipo) {
 		this.nombre = tipo;
 	}
