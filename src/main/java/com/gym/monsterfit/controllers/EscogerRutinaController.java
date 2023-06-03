a0ebd1d6b5b6b7d842b4f83e3751b2acb457a01e
KevinSarmiento07 <kevinandressasi@ufps.edu.co>
2023-06-03 01:51:59 -0500

    controlador creado, y vistas acomodadas

--- src/main/java/com/gym/monsterfit/controllers/EscogerRutinaController.java
+++ src/main/java/com/gym/monsterfit/controllers/EscogerRutinaController.java
@@ -3,11 +3,15 @@ package com.gym.monsterfit.controllers;
 import com.gym.monsterfit.entities.TipoEntity;
 import com.gym.monsterfit.repositories.TipoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -20,8 +24,24 @@ public class EscogerRutinaController {
     @GetMapping
     public String mostrarFormulario(Model model) {
         List<TipoEntity> tiposRutina = tipoRepository.findAll();
+        
         model.addAttribute("tiposRutina", tiposRutina);
-        return "admin/agregarEjercicioRutina";
+        return "admin/asignarRutina";
+    }
+    
+    
+    
+    @PostMapping
+    public String procesarSeleccionTipoRutina(@RequestParam("selectedOption") String rutina, @RequestParam("inputDate") @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate fecha1, Model modal) {
+    	TipoEntity tipoEntity = tipoRepository.getReferenceById(Integer.parseInt(rutina));
+    	System.out.println(tipoEntity.getNombre());
+    	System.out.println(fecha1);
+    	
+    	
+    	modal.addAttribute("tipoEntity", tipoEntity);
+    	modal.addAttribute("fecha", fecha1);
+    	
+    	return "admin/agregarEjercicioRutina";
     }
 
 
