3ea1984476f3768f307ca61a9008180b9fc43e6c
KevinSarmiento07 <kevinandressasi@ufps.edu.co>
2023-06-03 00:09:03 -0500

    Subiendo a mi rama

--- src/main/java/com/gym/monsterfit/controllers/EscogerRutinaController.java
+++ src/main/java/com/gym/monsterfit/controllers/EscogerRutinaController.java
@@ -6,16 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/rutina")
 public class EscogerRutinaController {
 
     @Autowired
     TipoRepository tipoRepository;
 
-    @GetMapping("/tipoRutina")
+    @GetMapping
     public String mostrarFormulario(Model model) {
         List<TipoEntity> tiposRutina = tipoRepository.findAll();
         model.addAttribute("tiposRutina", tiposRutina);
