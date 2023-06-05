b125a5cd06d4e8f10995eb341726528cda1cc466
josegabrielfc1152085 <josegabrielfc@ufps.edu.co>
2023-06-05 02:24:19 -0500

    validaciones en la vista asignarRutina

--- src/main/java/com/gym/monsterfit/repositories/EjercicioRepository.java
+++ src/main/java/com/gym/monsterfit/repositories/EjercicioRepository.java
@@ -1,11 +1,16 @@
 package com.gym.monsterfit.repositories;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.gym.monsterfit.entities.EjercicioEntity;
+import com.gym.monsterfit.entities.RutinaEntity;
 
 @Repository
 public interface EjercicioRepository extends JpaRepository<EjercicioEntity, Integer>{
 
     EjercicioEntity findByNombre(String nombre);
+
+    Optional<EjercicioEntity> findById(Long id);
 }
--- src/main/java/com/gym/monsterfit/repositories/RutinaRepository.java
+++ src/main/java/com/gym/monsterfit/repositories/RutinaRepository.java
@@ -1,7 +1,9 @@
 package com.gym.monsterfit.repositories;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.gym.monsterfit.entities.RutinaEntity;
 
 public interface RutinaRepository extends JpaRepository<RutinaEntity, Integer>{
-
+	Optional<RutinaEntity> findById(Long id);
 }
