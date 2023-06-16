1c93f4c75ccd7ae0e4bc6b1f9cb59c5456c7af13
Gibson-Arbey <gibsonarbey@gmail.com>
2023-06-15 20:48:56 -0500

    Se quitan los archivos e imports que no se usan

--- src/main/java/com/gym/monsterfit/controllers/EjercicioController.java
+++ src/main/java/com/gym/monsterfit/controllers/EjercicioController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
